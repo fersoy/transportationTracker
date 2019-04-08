a2dcc2449baca65c00e9772d764da0aebc21e2fb
Fatih Ersoy <f.ersoytx@gmail.com>
2019-04-07 23:43:11 -0500

    initial commit

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+
+ $(".animated").css("color", "yellow")
+.slideUp(2000)
+.slideDown(2000);
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBkRZsDhKVdf1uhqBV3pgTdsWhJOUJApUw",
+    authDomain: "employeedatamanagement-31059.firebaseapp.com",
+    databaseURL: "https://employeedatamanagement-31059.firebaseio.com",
+    projectId: "employeedatamanagement-31059",
+    storageBucket: "employeedatamanagement-31059.appspot.com",
+    messagingSenderId: "580318130337"
+  };
+  firebase.initializeApp(config);
